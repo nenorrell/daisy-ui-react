d25d9f26f0b0e72f9c240efea5e8b94db63b5ecb
Nick Norrell <norrell.nick@gmail.com>
2023-01-11 01:58:47 -0600

    Cleanup some stragglers in the IAlert interface

--- src/components/alert/Alert.tsx
+++ src/components/alert/Alert.tsx
@@ -3,8 +3,6 @@ import { StatusVariant } from "../../@types/Daisy";
 import ColorMap from "../../modules/ColorMap";
 
 interface IAlert {
-    buttonText ?:string
-    /** Use className for modifiers like btn-ghost, btn-circle, etc */
     className ?:string
     variant ?:StatusVariant
     onClick ?:MouseEventHandler
